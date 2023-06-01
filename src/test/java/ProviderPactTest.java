e8e54cdd4a15147411527e010b6fdb592d62edc9
milten <milten198@gmail.com>
2023-06-01 20:23:37 +0200

    pact test for provider

--- src/test/java/ProviderPactTest.java
+++ src/test/java/ProviderPactTest.java
@@ -1,33 +1,31 @@
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationExtension;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import org.example.Main;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @Provider("Provider") // replace with your provider name
 @PactBroker(host = "localhost", port = "9292") // replace with your Pact Broker host and port
-//@ExtendWith(PactVerificationExtension.class)
-//@IgnoreNoPactsToVerify
+@ExtendWith(PactVerificationExtension.class)
+@IgnoreNoPactsToVerify
 public class ProviderPactTest {
 
     private ConfigurableApplicationContext context;
 
-    @BeforeEach
-    void before() {
-        context = SpringApplication.run(Main.class);
-    }
-
-    @AfterEach
-    void after() {
-        context.stop();
-    }
+//    @BeforeEach
+//    void before() {
+//        context = SpringApplication.run(Main.class);
+//    }
+//
+//    @AfterEach
+//    void after() {
+//        context.stop();
+//    }
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
